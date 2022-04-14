be1edbdec9d34c55d1a51db829d1576f7d09cb95
Sachie Terada <tcams.41215@gmail.com>
2022-04-14 22:19:37 +0900

    added C to F Conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Conversion />
       </header>
     </div>
   );
 }
 
 export default App;
+
+function Conversion() {
+  const [celsius, setCelsius] = useState("");
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  return (
+    <>
+      <div className="flex flex-row space-x-10">
+        <div className="flex flex-col space-y-20">
+          <div className="flex-flex-col space-y-4">
+            <div>
+              Celsius:
+              <input
+                type="text"
+                value={celsius}
+                onChange={(e) => {
+                  let c = e.target.value;
+                  let f = parseInt(c) * 1.8 + 32;
+
+                  setCelsius(c);
+                  setFahrenheit(f.toString());
+                }}
+                className="text-slate-800"
+              />
+            </div>
+          </div>
+          <div className="flex-flex-col space-y-4">
+            <div>
+              Fahrenheit:
+              <input
+                type="text"
+                value={fahrenheit}
+                onChange={(e) => {
+                  let f = e.target.value;
+                  let c = ((parseInt(f) - 32) * 5) / 9;
+
+                  setFahrenheit(f);
+                  setCelsius(c.toString());
+                }}
+                className="text-slate-800"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
