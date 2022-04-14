610387f9e653b4bd6f66c160d8769f6a7205fd60
Sachie Terada <tcams.41215@gmail.com>
2022-04-14 23:00:52 +0900

    removed some import not using &  changed className

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Temperature } from "./Temperature";
 
 function App() {
   return (
-    <div className="App">
+    <div className="base">
       <Temperature />
     </div>
   );
