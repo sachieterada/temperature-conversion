376d778b547b39df44f96e3d4811e99555e5284f
Sachie Terada <tcams.41215@gmail.com>
2022-04-15 21:48:55 +0900

    added hader title

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,7 +3,10 @@ import ModeSwitch from "./ModeSwitch";
 export default function Header() {
   return (
     <>
-      <div className="w-full max-w-7xl mx-auto top-0 py-6 px-6 flex justify-end">
+      <div className="w-full max-w-7xl mx-auto top-0 py-6 px-6 flex flex-row justify-between items-center">
+        <h1 className="text-slate-800 dark:text-slate-400 text-3xl font-semibold">
+          Temperature Conversion
+        </h1>
         <ModeSwitch />
       </div>
     </>
