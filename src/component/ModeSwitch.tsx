c2bbe821052cadfc1c319638e3c09f01e220746a
Sachie Terada <tcams.41215@gmail.com>
2022-04-15 21:49:12 +0900

    removed Icon from bg

--- src/component/ModeSwitch.tsx
+++ src/component/ModeSwitch.tsx
@@ -11,14 +11,13 @@ export default function ModeSwitch() {
         checked={isDarkMode}
         onChange={modeSwitch}
         className={`${isDarkMode ? "bg-slate-700" : "bg-cyan-600"}
-          relative flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 flex justify-center items-center`}
+          relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Dark Mode Switch</span>
-        {isDarkMode && <SunIcon className="w-5 h-5 text-slate-400" />}
 
         <span
           aria-hidden="true"
-          className={`${isDarkMode ? "translate-x-9" : "translate-x-0 -ml-2"}
+          className={`${isDarkMode ? "translate-x-9" : "translate-x-0"}
             pointer-events-none h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200 flex justify-center items-center`}
         >
           {isDarkMode ? (
@@ -27,8 +26,6 @@ export default function ModeSwitch() {
             <SunIcon className="w-5 h-5 text-cyan-600" />
           )}
         </span>
-
-        {!isDarkMode && <MoonIcon className="w-5 h-5 text-cyan-300 ml-2" />}
       </Switch>
     </>
   );
