77992ba558d45555e5ec89961585f07372a29b02
Sachie Terada <tcams.41215@gmail.com>
2022-04-16 11:10:39 +0900

    changed text size  for SP

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <div className="w-full max-w-7xl mx-auto top-0 py-6 px-6 flex flex-row justify-between items-center">
-        <h1 className="text-slate-800 dark:text-slate-400 text-3xl font-semibold">
+        <h1 className="text-slate-800 dark:text-white text-3xl md:text-4xl font-semibold">
           Temperature Conversion
         </h1>
         <ModeSwitch />
