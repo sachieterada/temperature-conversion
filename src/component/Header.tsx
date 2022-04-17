0eb3fd351bb7b3e3316ab389603799931fc0429c
Sachie Terada <tcams.41215@gmail.com>
2022-04-17 15:42:47 +0900

    added logo

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,10 +1,12 @@
+import { RefreshIcon } from "@heroicons/react/outline";
 import ModeSwitch from "./ModeSwitch";
 
 export default function Header() {
   return (
     <>
       <div className="w-full max-w-7xl mx-auto top-0 py-6 px-6 flex flex-row justify-between items-center">
-        <h1 className="text-slate-800 dark:text-white text-3xl md:text-4xl font-semibold">
+        <h1 className="text-slate-800 dark:text-white text-3xl md:text-4xl font-semibold flex flex-row items-center">
+          <RefreshIcon className="h-8 w-8 mr-2" />
           Temperature Conversion
         </h1>
         <ModeSwitch />
