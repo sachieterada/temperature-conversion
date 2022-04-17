9a5fc47a12ea1e379efb4db35d54ba0cfb966a44
Sachie Terada <tcams.41215@gmail.com>
2022-04-17 15:43:13 +0900

    extracted Input component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,30 @@ function fahrenheitToCelsius(f: number) {
   return ((f - 32) * 5) / 9;
 }
 
+function Input({
+  value,
+  onChange,
+  error,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+  error: boolean;
+}) {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+      className={`max-w-xs md:max-w-none text-slate-800 dark:text-white py-4 text-center text-3xl border-b-4 mb-4 bg-white dark:bg-slate-900
+    ${
+      error
+        ? "text-red-400 dark:text-red-400 border-red-400"
+        : "border-cyan-400"
+    }`}
+    />
+  );
+}
+
 export default function App() {
   let initial = 0;
   const [celsius, setCelsius] = useState(initial.toString());
@@ -25,23 +49,22 @@ export default function App() {
         <Header />
 
         <div className="flex-1 w-full max-w-7xl mx-auto py-5 md:py-8 flex justify-center items-center text-center">
-          <div className="flex flex-col md:flex-row md:space-x-20 space-y-10 md:space-y-0">
+          <div className="flex flex-col md:flex-row md:space-x-20 space-y-20 md:space-y-0">
             <div className="flex flex-col">
-              <input
-                type="text"
+              <Input
                 value={celsius}
-                onChange={(e) => {
-                  let c = parseFloat(e.target.value);
+                onChange={(value) => {
+                  let c = parseFloat(value);
                   setError(false);
 
-                  // ignore blank value
-                  if (e.target.value === "") {
+                  // ignore blank value, it's not an error
+                  if (value === "") {
                     setCelsius("");
                     setFahrenheit("");
                     return;
                   }
 
-                  setCelsius(e.target.value);
+                  setCelsius(value);
 
                   if (isNaN(c)) {
                     setError(true);
@@ -50,33 +73,26 @@ export default function App() {
                     setFahrenheit(f);
                   }
                 }}
-                className={`
-                  max-w-xs md:max-w-none text-slate-800 dark:text-white py-4 text-center text-3xl border-b-4 mb-4 bg-white dark:bg-slate-900
-                  ${
-                    error
-                      ? "text-red-400 dark:text-red-400 border-red-400"
-                      : "border-cyan-400"
-                  }`}
+                error={error}
               />
 
               <div className="font-semibold text-xl">Celsius</div>
             </div>
             <div className="flex flex-col">
-              <input
-                type="text"
+              <Input
                 value={fahrenheit}
-                onChange={(e) => {
-                  let f = parseFloat(e.target.value);
+                onChange={(value) => {
+                  let f = parseFloat(value);
                   setError(false);
 
-                  // ignore blank value
-                  if (e.target.value === "") {
+                  // ignore blank value, it's not an error
+                  if (value === "") {
                     setCelsius("");
                     setFahrenheit("");
                     return;
                   }
 
-                  setFahrenheit(e.target.value);
+                  setFahrenheit(value);
 
                   if (isNaN(f)) {
                     setError(true);
@@ -85,13 +101,7 @@ export default function App() {
                     setCelsius(c);
                   }
                 }}
-                className={`
-                max-w-xs md:max-w-none text-slate-800 dark:text-white py-4 text-center text-3xl border-b-4 mb-4 bg-white dark:bg-slate-900
-                ${
-                  error
-                    ? "text-red-400 dark:text-red-400 border-red-400"
-                    : "border-cyan-400"
-                }`}
+                error={error}
               />
 
               <div className="font-semibold text-xl">Fahrenheit</div>
